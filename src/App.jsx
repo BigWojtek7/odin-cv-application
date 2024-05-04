958f2e079d8c0f1adabd3381bf972410f96f7d5f
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-05-04 09:16:46 +0200

    Add onChange functions to inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { initial } from "./initialObj";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [person, setPerson] = useState({
-    firstName: "John",
-    lastName: "Andrews",
-    age: 100,
-  });
 
-  const handleFirstNameChange = () => console.log()
-  const handleLastNameChange = () => console.log()
+  const [person, setPerson] = useState(initial);
+  const [status, setStatus] = useState("typing");
 
-  const handleSubmit = () => console.log()
+  const handleChange = () => console.log("2222");
+
+
+  const handleSubmit = () => console.log();
   return (
     <>
-    <form className="cv-input" onSubmit={handleSubmit}>
-      <h1>CV Application</h1>
-      <h2>Personal Information:</h2>
-      <label>
-        First name:{" "}
-        <input value={person.firstName} onChange={handleFirstNameChange} />
-      </label>
-      <label>
-        Last name:{" "}
-        <input value={person.lastName} onChange={handleLastNameChange} />
-      </label>
-      <label>
-        E-mail:{" "}
-        <input value={person.lastName} onChange={handleLastNameChange} />
-      </label>
-      <label>
-        Phone number:{" "}
-        <input value={person.lastName} onChange={handleLastNameChange} />
-      </label>
-      <h2>Educational experience:</h2>
-      <label>
-        School name:{" "}
-        <input value={person.lastName} onChange={handleLastNameChange} />
-      </label>
-      <label>
-        Title of study:{" "}
-        <input value={person.lastName} onChange={handleLastNameChange} />
-      </label>
-      <label>
-        Date of study:{" "}
-        <input value={person.lastName} onChange={handleLastNameChange} />
-      </label>
-      <h2>Practical Experience</h2>
-      <label>
-        Company name:{" "}
-        <input value={person.lastName} onChange={handleLastNameChange} />
-      </label>
-      <label>
-        Position title:{" "}
-        <input value={person.lastName} onChange={handleLastNameChange} />
-      </label>
-      <label>
-        Main responsibilities:{" "}
-        <input value={person.lastName} onChange={handleLastNameChange} />
-      </label>
-      <label>
-        Date of work:{" "}
-        <input value={person.lastName} onChange={handleLastNameChange} />
-      </label>
-      <button>Submit</button>
+      <form className="cv-input" onSubmit={handleSubmit}>
+        <h1>CV Application</h1>
+        <h2>Personal Information:</h2>
+        <label>
+          First name:{" "}
+          <input value={person.firstName} onChange={e => setPerson({...person, firstName: e.target.value})} />
+        </label>
+        <label>
+          Last name:{" "}
+          <input value={person.lastName} onChange={e => setPerson({...person, lastName: e.target.value})} />
+        </label>
+        <label>
+          E-mail:{" "}
+          <input value={person.email} onChange={e => setPerson({...person, email: e.target.value})} />
+        </label>
+        <label>
+          Phone number:{" "}
+          <input value={person.phone} onChange={e => setPerson({...person, phone: e.target.value})} />
+        </label>
+        <h2>Educational experience:</h2>
+        <label>
+          School name:{" "}
+          <input value={person.schoolName} onChange={e => setPerson({...person, schoolName: e.target.value})} />
+        </label>
+        <label>
+          Title of study:{" "}
+          <input value={person.titleOfStudy} onChange={e => setPerson({...person, titleOfStudy: e.target.value})} />
+        </label>
+        <label>
+          Date of study:{" "}
+          <input value={person.dateOfStudy} onChange={e => setPerson({...person, dateOfStudy: e.target.value})} />
+        </label>
+        <h2>Practical Experience</h2>
+        <label>
+          Company name:{" "}
+          <input value={person.companyName} onChange={e => setPerson({...person, companyName: e.target.value})} />
+        </label>
+        <label>
+          Position title:{" "}
+          <input value={person.positionTitle} onChange={e => setPerson({...person, positionTitle: e.target.value})} />
+        </label>
+        <label>
+          Main responsibilities:{" "}
+          <input value={person.mainResponsibilities} onChange={e => setPerson({...person, mainResponsibilities: e.target.value})} />
+        </label>
+        <label>
+          Date of work:{" "}
+          <input value={person.dateOfWork} onChange={e => setPerson({...person, dateOfWork: e.target.value})} />
+        </label>
+        <button>Submit</button>
 
-      <p>
-        Your CV is: <b>{}</b>
-      </p>
-    </form>
+        <p>
+          Your CV is: <b>{}</b>
+        </p>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/initialObj.js
@@ -0,0 +1,15 @@
+export const initial = {
+
+  firstName: "John",
+  lastName: "Andrews",
+  email: 100,
+  phone: "",
+
+  schoolName: "",
+  titleOfStudy: "",
+  dateOfStudy: "",
+  companyName: "",
+  positionTitle:"",
+  mainResponsibilities: "",
+  dateOfWork: "",
+};
